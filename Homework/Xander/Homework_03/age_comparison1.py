50ccc546944f992fa1159c0dce7adac917434d18
XanderCatR999 <81835052+XanderCatR999@users.noreply.github.com>
2025-04-28 09:45:29 -0700

    Add files via upload

---
+++ Homework/Xander/Homework_03/age_comparison1.py
@@ -0,0 +1,36 @@
+# Store ages in variables
+my_age =int(input('whats your age?'))
+friend_age =int(input('whats your friends age?'))
+
+# Compare ages using comparison operators
+if my_age > friend_age:
+    print("I am older than my friend.")
+elif my_age < friend_age:
+    print("My friend is older than me.")
+else:
+    print("We are the same age.")
+
+
+
+
+
+
+
+#SCORE STORING
+
+score = 88
+passing_grade = 70
+
+if score >= passing_grade:
+  print("You have Passed")
+else:
+  print("You have Failed")
+
+
+
+
+
+
+
+
+
