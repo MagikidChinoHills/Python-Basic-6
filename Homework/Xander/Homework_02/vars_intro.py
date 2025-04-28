0d1e86f8c9a67174ec250413e49313081a7f5a2d
XanderCatR999 <81835052+XanderCatR999@users.noreply.github.com>
2025-04-28 09:33:29 -0700

    Add files via upload

---
+++ Homework/Xander/Homework_02/vars_intro.py
@@ -0,0 +1,17 @@
+# Prompt the user for their age.
+age_str = input("Please enter your age: ")
+
+
+
+
+
+#Prompt for Temperature
+celsius = float(input("Enter the temperature in Celsius: "))
+
+# Convert the input string to an integer.
+age = int(age_str)
+
+print("Your age is:", age)
+print('The Temperature is:', celsius )
+
+
